aa62a9bddc885100cc839a8280a170f853b7defb
Josh Lubaway <josh.lubaway@gmail.com>
2010-09-26 23:05:58 -0700

    Adding jQuery

--- lib/client.rb
+++ lib/client.rb
@@ -2,6 +2,20 @@ require 'sinatra/base'
 
 class Client < Sinatra::Base
 
+  helpers do
+    def jquery_tag(version = '1.4.2')
+      if ENV['RACK_ENV'] == 'development'
+        path = "/javascripts/vendor/jquery-#{version}.js"
+      else
+        path = 'http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js'
+      end
+
+      "<script type=\"text/javascript\" src=\"#{path}\"></script>"
+    end
+  end
+
+  set :public, 'public'
+
   get '/' do
     erb :index
   end
