621ecff3587af8e137671fadf825f71fe1013184
Josh Lubaway <josh@zendesk.com>
2010-10-04 15:02:07 -0700

    Pulling 'commits' from the 'API' and displaying them.

--- lib/api.rb
+++ lib/api.rb
@@ -4,7 +4,7 @@ class Api < Sinatra::Base
 
   get '/commits.json' do
     content_type 'application/json'
-    '[{"author":"Somebody", "commitMessage":"I committed something"}]'
+    '[{"author":"Somebody", "message":"I committed something"}]'
   end
 
 end
