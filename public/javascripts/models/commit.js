531fe00b7ff1e3dbbf332a4601baf480b4e96ade
Josh Lubaway <josh.lubaway@gmail.com>
2010-11-10 14:50:33 -0800

    Added a totally awesome key value store to Commit.js

--- public/javascripts/models/commit.js
+++ public/javascripts/models/commit.js
@@ -1,5 +1,27 @@
 var Commit = function(attributes) {
   this.id = attributes.id;
-  this.accepted = localStorage['commit.' + this.id + '.accepted'] == 'true';
-  this.subject = attributes.message.split('\n')[0].substr(0, 50)
+  this.accepted = this.attr('accepted') == 'true';
+  this.message = attributes.message;
+};
+
+Commit.prototype = {
+
+  accept: function() {
+    this.attr('accepted', true);
+  },
+
+  subject: function() {
+    return this.message.split('\n')[0].substr(0, 50);
+  },
+
+  attr: function(key, value) {
+    var key = ['commit', this.id, key].join('.');
+
+    if (value) {
+      localStorage[key] = value;
+    }
+
+    return localStorage[key];
+  }
+
 };
